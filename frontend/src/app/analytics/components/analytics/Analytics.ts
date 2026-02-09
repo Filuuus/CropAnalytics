ebaf44bf24113d014325e5c394fd6cb70bafd8ed
Filuuus <Filuuus>
2026-02-09 09:25:54 +0000

    Pushed from Locofy Builder

---
+++ frontend/src/app/analytics/components/analytics/Analytics.ts
@@ -0,0 +1,25 @@
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { Router } from "@angular/router";
+import { Header } from "../../../shared/components/header/Header";
+import { Main } from "../main/Main";
+import { Footer } from "../../../shared/components/footer/Footer";
+
+@Component({
+  selector: "analytics",
+
+  imports: [Header, Main, Footer],
+  templateUrl: "./Analytics.html",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { "[style.display]": "'contents'" },
+})
+export class Analytics {
+  constructor(private router: Router) {}
+
+  onHomeTextClick() {
+    this.router.navigate(["/1920w-light"]);
+  }
+
+  onGoToDashboardClick() {
+    this.router.navigate(["/1920w-light2"]);
+  }
+}
