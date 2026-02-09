4e291bce3e1128eb6eb64bbd91a0af34d90da875
Juan Pablo Martin Hurtado <juanpapsp1@gmail.com>
2026-02-09 02:19:44 -0600

    Configuración de PostgreSQL con PosGIS
    Archivo docker-compose para facilitar la configuración de la db y su administración con pgAdmin.
    
    Configuración de CORS para aceptar requests desde el servidor de desarrollo frontend.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,10 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.gis',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,8 +79,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Motor GIS
+        'NAME': 'crop_analytics',
+        'USER': 'usuario_maiz',
+        'PASSWORD': 'password_seguro_dev',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -115,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+    'http://127.0.0.1:4200',
+]
